44b58418dda91e74f4513bbef7378d09445de082
Kyle Kearney <kyledkearney@gmail.com>
2023-02-28 11:00:12 -0700

    setup tailwind css / ui

--- app/root.tsx
+++ app/root.tsx
@@ -7,12 +7,17 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import stylesheet from "~/tailwind.css";
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
   title: "New Remix App",
   viewport: "width=device-width,initial-scale=1",
 });
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: stylesheet },
+];
 
 export default function App() {
   return (
