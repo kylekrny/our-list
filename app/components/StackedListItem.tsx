8b365d8028939d5d7a73f7f5aae876a8713f8891
Kyle Kearney <kyledkearney@gmail.com>
2023-03-02 09:24:01 -0700

    have it updating the list item votes

--- app/components/StackedListItem.tsx
+++ app/components/StackedListItem.tsx
@@ -1,6 +1,7 @@
+import { useEffect, useState} from "react";
 import { ArrowUpIcon, ArrowDownIcon } from "@heroicons/react/20/solid";
-
-
+import { Form } from "@remix-run/react";
+import { useFetcher } from "@remix-run/react";
 interface Tag {
     id: string;
     name: string;
@@ -16,45 +17,59 @@ interface ListItem {
 
 
 const numberFormatter = (number: number) => {
+
   return new Intl.NumberFormat("en-US", { maximumSignificantDigits: 3 }).format(
     number
   );
 };
 
 export default function StackedListItem({listItem, idx}: {listItem: ListItem, idx: number}) {
-  return (
-          <li key={listItem.id}>
-            <div className="px-4 py-4 sm:px-6">
-              <div className="flex items-center justify-between">
-                <p className="truncate text-sm font-medium text-cyan-800">
-                  <span className="text-gray-500">#{idx + 1}</span>{" "}
-                  {listItem.title}
-                </p>
-                {/* <div className="ml-2 flex flex-shrink-0">
+    const [listItemState, setListItemState] = useState(listItem)
+    const fetcher = useFetcher();
+
+    useEffect(() => {
+        setListItemState({
+            ...listItemState,
+            ...fetcher.data,
+        })
+    }, [fetcher])
+
+    console.log(listItemState);
+    
+    return (
+      <li key={listItem.id}>
+        <div className="px-4 py-4 sm:px-6">
+          <div className="flex items-center justify-between">
+            <p className="truncate text-sm font-medium text-cyan-800">
+              <span className="text-gray-500">#{idx + 1}</span> {listItem.title}
+            </p>
+            {/* <div className="ml-2 flex flex-shrink-0">
                   <p className="inline-flex rounded-full bg-green-100 px-2 text-xs font-semibold leading-5 text-green-800">
                     {listItem.type}
                   </p>
                 </div> */}
-              </div>
-              <div className="mt-2 sm:flex sm:justify-between">
-                <div className="sm:flex">
-                  <p className="text-sm text-gray-500">
-                    {listItem.description}
-                  </p>
-                </div>
-                <div className="mt-2 flex items-center text-sm text-gray-500 sm:mt-0">
-                  <button>
-                    <ArrowUpIcon className="mr-1 h-5 w-5 flex-shrink-0 text-gray-400 hover:text-green-500" />
-                  </button>
-                  <p className="flex items-center text-sm text-gray-500">
-                    {numberFormatter(listItem.votes)}
-                  </p>
-                  <button>
-                    <ArrowDownIcon className=" ml-1 h-5 w-5 flex-shrink-0 text-gray-400 hover:text-rose-600" />
-                  </button>
-                </div>
-              </div>
+          </div>
+          <div className="mt-2 sm:flex sm:justify-between">
+            <div className="sm:flex">
+              <p className="text-sm text-gray-500">{listItem.description}</p>
             </div>
-          </li>
-  );
+            <fetcher.Form
+              action="/list-item/update"
+              method="put"
+              className="mt-2 flex items-center text-sm text-gray-500 sm:mt-0"
+            >
+              <button type="submit" name="up-vote" value={listItem.id}>
+                <ArrowUpIcon className="mr-1 h-5 w-5 flex-shrink-0 text-gray-400 hover:text-green-500" />
+              </button>
+              <p className="flex items-center text-sm text-gray-500">
+                {numberFormatter(listItem.votes)}
+              </p>
+              <button type="submit" name="down-vote" value={listItem.id}>
+                <ArrowDownIcon className=" ml-1 h-5 w-5 flex-shrink-0 text-gray-400 hover:text-rose-600" />
+              </button>
+            </fetcher.Form>
+          </div>
+        </div>
+      </li>
+    );
 }
---
+++ app/routes/list-item/update.ts
@@ -0,0 +1,18 @@
+import type { ActionArgs } from "@remix-run/node";
+
+
+export const action = async ({ request }: ActionArgs) => {
+  const formData = await request.formData();
+
+  const upVote = formData.get("up-vote");
+  const downVote = formData.get("down-vote");
+
+  if (upVote) {
+    console.log("this works")
+    return {upVote: true};
+  } else if (downVote) {
+    return {downVote: true};
+  } else {
+    throw new Error("vote not counted")
+  }
+};
